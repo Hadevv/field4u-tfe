4867255db72181c9e51e7513ac494c20c15d941a
Hadevv <ademeure29@gmail.com>
2025-03-29 12:19:56 +0100

    🐛fix(error): update HeaderBase component to reflect unauthenticated state on error page

--- app/error.tsx
+++ app/error.tsx
@@ -6,7 +6,7 @@ import { Page400 } from "@/features/page/Page400";
 export default function ErrorPage() {
   return (
     <div className="relative flex min-h-screen flex-col">
-      <HeaderBase />
+      <HeaderBase isAuthenticated={false} />
       <div className="flex flex-1 items-center justify-center">
         <Page400 />
       </div>
