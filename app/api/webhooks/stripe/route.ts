813ec89b67e936c6cdf36a542b425b02787ff9f9
Hadevv <ademeure29@gmail.com>
2025-04-24 21:25:07 +0200

    🐛 fix(announcements): add "use client" for the component and fix Stripe header imports

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,7 +1,7 @@
 import { env } from "@/lib/env";
 import { logger } from "@/lib/logger";
 import { stripe } from "@/lib/stripe";
-import { headers } from "next/dist/client/components/headers";
+import { headers } from "next/headers";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import type Stripe from "stripe";
