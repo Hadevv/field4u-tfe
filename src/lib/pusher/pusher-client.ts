0ed534554e474967d99151cc0a27c3e882a28470
Hadevv <ademeure29@gmail.com>
2025-05-09 18:40:42 +0200

    ♻️ chore(pusher): reorganize environment setup and add Pusher configuration and .gitignore, .env.exemple, readme

--- src/lib/pusher/pusher-client.ts
+++ src/lib/pusher/pusher-client.ts
@@ -1,7 +1,8 @@
 import Pusher from "pusher-js";
+import { env } from "@/lib/env";
 
 // config pusher client
-export const pusherClient = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+export const pusherClient = new Pusher(env.NEXT_PUBLIC_PUSHER_KEY || "", {
+  cluster: env.NEXT_PUBLIC_PUSHER_CLUSTER || "eu",
   forceTLS: true,
 });
--- src/lib/pusher/pusher-server.ts
+++ src/lib/pusher/pusher-server.ts
@@ -1,10 +1,11 @@
 import Pusher from "pusher";
+import { env } from "@/lib/env";
 
 // config pusher serveur
 export const pusherServer = new Pusher({
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.PUSHER_KEY!,
-  secret: process.env.PUSHER_SECRET!,
-  cluster: process.env.PUSHER_CLUSTER!,
+  appId: env.PUSHER_APP_ID || "",
+  key: env.PUSHER_KEY || "",
+  secret: env.PUSHER_SECRET || "",
+  cluster: env.PUSHER_CLUSTER || "eu",
   useTLS: true,
 });
